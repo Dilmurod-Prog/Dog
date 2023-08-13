fdb13bd30539eb4176afb9dc50dfab8fec6f9c9d
Dilmurod-Prog <dilmurodfreelance@gmail.com>
2023-08-13 19:10:34 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.left = "0";
+}
+
+function closeNavbar() {
+    document.getElementById("navbar-responsive").style.left = "-100%";
+}
+  
